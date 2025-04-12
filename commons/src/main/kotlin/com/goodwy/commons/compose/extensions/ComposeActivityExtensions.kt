1e346fb2d3f4daa537ad462e549946529839159c
Saeed Sql <saeed.sql02@gmail.com>
2025-04-12 12:37:44 +0330

    Revert "Revert "removed fakeApp""
    This reverts commit 1f4794a6470e0917e510fa73b9fc07691229ba3d.

--- commons/src/main/kotlin/com/goodwy/commons/compose/extensions/ComposeActivityExtensions.kt
+++ commons/src/main/kotlin/com/goodwy/commons/compose/extensions/ComposeActivityExtensions.kt
@@ -10,22 +10,7 @@ import com.devgroup.commons.extensions.launchViewIntent
 
 @Composable
 fun FakeVersionCheck() {
-    val context = LocalContext.current
-    val confirmationDialogAlertDialogState = rememberAlertDialogState().apply {
-        DialogMember {
-            ConfirmationAlertDialog(
-                alertDialogState = this,
-                message = FAKE_VERSION_APP_LABEL,
-                positive = R.string.ok,
-                negative = null
-            ) {
-                context.getActivity().launchViewIntent(DEVELOPER_PLAY_STORE_URL)
-            }
-        }
-    }
-    LaunchedEffect(Unit) {
-        context.fakeVersionCheck(confirmationDialogAlertDialogState::show)
-    }
+
 }
 
 @Composable
